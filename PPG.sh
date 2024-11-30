fd908daac7f379c68ff43f51bade3c45739195fe
[ROMA] <136695262+ROMAsofthub@users.noreply.github.com>
2024-11-30 21:17:03 +0100

    Update PPG.sh

--- PPG.sh
+++ PPG.sh
@@ -51,13 +51,13 @@ echo "fi" >> $SETUP_FILE
 echo "echo -e \"\${YELLOW}↻ Creating project directory: \$PROJECT_DIR...\"" >> $SETUP_FILE
 echo "mkdir -p \$PROJECT_DIR || error_exit \"Failed to create project directory: $PROJECT_DIR\" && success_msg \"Project directory created: $PROJECT_DIR\"" >> $SETUP_FILE
 
-# Create a virtual environment inside the project directory
+# Create a virtual environment
 echo "echo -e \"\${YELLOW}↻ Creating virtual environment: $VENV_DIR...\"" >> $SETUP_FILE
 echo "python3 -m venv \$PROJECT_DIR/$VENV_DIR || error_exit \"Failed to create virtual environment: $VENV_DIR\" && success_msg \"Virtual environment created: $VENV_DIR\"" >> $SETUP_FILE
 
-# Activate the virtual environment temporarily
+# Activate the virtual environment temporaraly
 echo "echo -e \"\${YELLOW}↻ Activating virtual environment...\"" >> $SETUP_FILE
-echo "source \$PROJECT_DIR/$VENV_DIR/bin/activate || error_exit \"Failed to activate virtual environment temporarily: $VENV_DIR\" && success_msg \"Virtual environment activated temporarily: $VENV_DIR\"" >> $SETUP_FILE
+echo "source $PROJECT_DIR/$VENV_DIR/bin/activate || error_exit \"Failed to activate virtual environment temporarily: $VENV_DIR\" && success_msg \"Virtual environment activated temporarily: $VENV_DIR\"" >> $SETUP_FILE
 
 # Install modules
 for MODULE in "${MODULES[@]}"; do
@@ -80,13 +80,12 @@ done
 # Activate_venv file creation
 echo "echo -e \"\${YELLOW}↻ Creating file: activate_venv.sh...\${RED}\"" >> $SETUP_FILE
 echo "touch \$PROJECT_DIR/activate_venv.sh || error_exit \"Failed to create file: activate_venv.sh\" && success_msg \"File created: activate_venv.sh\"" >> $SETUP_FILE
-echo "chmod +x activate_venv.sh" >> $SETUP_FILE
-echo "cat<<EOF > activate_venv.sh" >> $SETUP_FILE
-echo "#!/bin/bash\"" >> $SETUP_FILE
+echo "cat<<EOF > ./$PROJECT_DIR/activate_venv.sh" >> $SETUP_FILE
+echo "#!/bin/bash" >> $SETUP_FILE
 echo "echo -e \"↻ Activating virtual environment...\"" >> $SETUP_FILE
-echo "source $PROJECT_DIR/$VENV_DIR/bin/activate" >> $SETUP_FILE
+echo "source $VENV_DIR/bin/activate" >> $SETUP_FILE
 echo "EOF" >> $SETUP_FILE
-echo "chmod +x activate_venv.sh" >> $SETUP_FILE
+echo "chmod +x ./$PROJECT_DIR/activate_venv.sh" >> $SETUP_FILE
 
 # Inform the user
 echo "echo -e \"\${GREEN}✓ Setup complete. Virtual environment, Modules, folders and files initialized in \$PROJECT_DIR.\${RESET}\"" >> $SETUP_FILE
@@ -99,5 +98,5 @@ chmod +x $SETUP_FILE
 echo "$SETUP_FILE has been generated. Run it to install everything"
 
 # Python Project Generator
-# Ver: 2.4
+# Ver: fix2.5
 # Not reserved. Please give [ROMA]software credit if you distribute it.
